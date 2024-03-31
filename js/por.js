e9da783bfd2500e73bd7f84b96f85226aeae9203
Mohammed-Mujib <wadmujib@gmail.com>
2024-03-31 14:27:48 +0200

    version 1.0.6 slides

--- js/por.js
+++ js/por.js
@@ -1,12 +1,30 @@
 let mode_but = document.getElementById("modes");
-//TODO:make the dark and light mode more cooler
+if (localStorage.getItem("dark_mode")) {
+  if (localStorage.getItem("dark_mode")==="on") {
+    document.body.classList.toggle("dark_mode");
+    mode_but.innerHTML =`<i class="fa-solid fa-sun"></i>`;
+  }
+}
 mode_but.onclick =function(){
   document.body.classList.toggle("dark_mode");
   if(document.body.classList.contains("dark_mode")){
   mode_but.innerHTML =`<i class="fa-solid fa-sun"></i>`;
+  localStorage.setItem("dark_mode","on")
   }
   else{
     mode_but.innerHTML =`<i class="fa-solid fa-moon"></i>`;
+    localStorage.setItem("dark_mode","off")
   }
+}
+let observer = new IntersectionObserver((enteries)=>{
+  enteries.forEach((entry)=>{
+    if(entry.isIntersecting){
+    entry.target.classList.add("show");
   }
-//TODO:make the show from the sides action
+    // else{
+    //   enyry.target.classList.remove("show");
+    // }
+  })
+});
+let hiddenelement = document.querySelectorAll(".hidden");
+hiddenelement.forEach((el)=>observer.observe(el));
