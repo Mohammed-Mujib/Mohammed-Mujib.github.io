3563e2dc4cb4adea5814da8889b50eab18e56dfd
Mohammed-Mujib <wadmujib@gmail.com>
2024-03-31 03:09:36 +0200

    version 1.0.5 dark and light mode

--- js/por.js
+++ js/por.js
@@ -1,13 +1,5 @@
 let mode_but = document.getElementById("modes");
-let mind = document.querySelector(".myHeader");
-let skil = document.querySelectorAll("svg");
-let skill_Pat = document.querySelectorAll("path");
-let i_list = document.querySelectorAll(".skill .skill_logo");
-let con = document.querySelectorAll(".socil")
-console.log(con);
-// let con = document.querySelectorAll("")
 //TODO:make the dark and light mode more cooler
-
 mode_but.onclick =function(){
   document.body.classList.toggle("dark_mode");
   if(document.body.classList.contains("dark_mode")){
